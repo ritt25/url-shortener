a38d49211d7cb740e3a29423f6eda93e0ec25bd3
Ritwik <ritwikguha25@gmail.com>
2026-03-16 19:19:47 +0530

    fix redis url protocol

--- src/cache.js
+++ src/cache.js
@@ -2,10 +2,6 @@ const redis = require("redis");
 
 const client = redis.createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
 });
 
 client.on("error", (err) => console.error("Redis error:", err));
